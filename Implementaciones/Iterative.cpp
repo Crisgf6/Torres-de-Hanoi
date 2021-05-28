4c8325c654b9a976e076ff18b6553d5a366607e6
Crisgf6 <72546175+Crisgf6@users.noreply.github.com>
2021-05-28 20:35:48 +0200

    Add files via upload

--- Implementaciones/Iterative.cpp
+++ Implementaciones/Iterative.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <math.h>
-#include <ctime> 
+#include <ctime>
+#include <vector>
+#include <numeric>
 
 using namespace std;
 
@@ -25,29 +27,34 @@ int direccion(int d,int n) { //Cada disco se mueve en la misma dirección CW=1, 
     return 2 - (n + d)%2;
 }
 
-void msg(int disco, int origen, int destino) {
-    cout<<"Mover el disco "<<disco+1<<" desde "<<origen+1<<" hasta "<<destino+1<<endl;
-}
-
 int main(){
-    unsigned t0, t1;
-    t0=clock();
-    
-    int n;
-    cout<<"Los Torres son 1 2 3\n";
-    cout<<"Cantidad de discos: ";
-    cin>>n;
-    int limit = pow(2,n)-1; //Numero de iteraciones 
-    for(int i=0;i<limit;i++){
-        int disco = disc(i); //Disco a mover
-        int origen = (movimientos(i,disco)*direccion(disco,n))%3; //Torre origen (en esta implementacion, la torre auxiliar es la 3)
-        int destino = (origen + direccion(disco,n))%3; //Torre destino
-        msg(disco,origen,destino);
+    vector<double> medias;
+    for(int n=3;n<30;n++){
+        vector<double> times;
+        for (int i=0; i<10; i++) {
+            int t0 = clock();
+            
+            int limit = pow(2,n)-1; //Numero de iteraciones 
+            for(int i=0;i<limit;i++){
+                int disco = disc(i); //Disco a mover
+                int origen = (movimientos(i,disco)*direccion(disco,n))%3; //Torre origen (en esta implementacion, la torre auxiliar es la 3)
+                int destino = (origen + direccion(disco,n))%3; //Torre destino
+            }
+            
+            int t1 = clock();
+            double time = (double(t1-t0)/CLOCKS_PER_SEC);
+            times.push_back(time);
+        }
+        //Cálculo de la media
+        double medium = accumulate(times.begin(), times.end(), 0)/times.size();
+        medias.push_back(medium);
     }
-    cout<<limit<<" movimientos"<<endl;
-    
-    t1 = clock();
-    double time = (double(t1-t0)/CLOCKS_PER_SEC);
-    cout << "Execution Time: " << time << endl;
+    int cont = 3;
+    for(int i=0;i<medias.size();i++){
+        cout << "Tiempo medio con " << cont << " discos = " << medias[i] << endl;
+        cont++;
+    }
     return 0;
 }
+
+
--- Implementaciones/Recursive.cpp
+++ Implementaciones/Recursive.cpp
@@ -1,35 +1,61 @@
 #include <iostream>
-#include <ctime> 
+#include <vector>
+#include <numeric>
+#include <ctime>
+#include <stack>
 
 using namespace std;
 
-void hanoi(int num, int a, int c, int b, int &cont){  //Se pasa el contador por referencia para poder mostrar el numero de movimientos al final del programa
-    if(num==1){
-        cout<<"Mover el disco "<<num<<" desde "<<a<<" hasta  "<<c<<endl;
-        cont++;
+void hanoi(int n, stack<int>& origen, stack<int>& destino, stack<int>& auxiliar) {
+    if (n == 1) {
+        int top = origen.top();
+        destino.push(top);
+        origen.pop();
+        return;
     }
-    else{
-        hanoi(num-1,a,b,c,cont);
-        cout<<"Mover el disco "<<num<<" desde "<<a<<" hasta  "<<c<<endl;
-        cont++;
-        hanoi(num-1,b,c,a,cont);
+    else {
+        //Mover n-1 discos de la torre origen a la torre auxiliar
+        hanoi(n - 1, origen, auxiliar, destino); //llamada recursiva
+        //Mover un disco de la torre origen a la torre destino
+        int top = origen.top();
+        destino.push(top);
+        origen.pop();
+        //Mover n-1 discos de la torre auxiliar a la torre destino
+        hanoi(n - 1, auxiliar, destino, origen);
     }
+    return;
 }
- 
-int main(){
-    unsigned t0, t1;
-    t0=clock();
-    
-    int n;
-    int cont = 0;
-    cout<<"Los Torres son 1 2 3\n";
-    cout<<"Cantidad de discos: ";
-    cin>>n;
-    hanoi(n,1,3,2,cont);
-    cout<<cont<<" movimientos"<<endl;
-    
-    t1 = clock();
-    double time = (double(t1-t0)/CLOCKS_PER_SEC);
-    cout << "Execution Time: " << time << endl;
+
+int main() {
+    vector<double> medias;
+    for (int n = 3;n <= 30;n++) {
+        vector<double> times;
+        for (int i = 0; i < 10; i++) {
+            int t0 = clock();
+
+            stack<int> origen;
+            stack<int> destino;
+            stack<int> auxiliar;
+
+            //llenando la pila de origen con los discos
+            for (int i = 0; i < n; i++) {
+                origen.push(n - i);
+            }
+
+            hanoi(n, origen, destino, auxiliar);
+
+            int t1 = clock();
+            double tiempo = (double(t1 - t0) / CLOCKS_PER_SEC);
+            times.push_back(tiempo);
+        }
+        //Cálculo de la media
+        double medium = accumulate(times.begin(), times.end(), 0) / times.size();
+        medias.push_back(medium);
+    }
+    int cont = 3;
+    for (int i = 0;i < medias.size();i++) {
+        cout << "Tiempo medio con " << cont << " discos = " << medias[i] << endl;
+        cont++;
+    }
     return 0;
 }
